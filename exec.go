033eeb5bbc2ffcc8f4fec15292aabaaebfcc051a
Robert Pajak <pellared@hotmail.com>
2020-10-28 22:43:02 +0100

    doc: TF

--- exec.go
+++ exec.go
@@ -6,6 +6,10 @@ import (
 	"strings"
 )
 
+// Exec runs the specified command and waits for it to complete.
+// The process stderr and stdout are piped to the Output.
+// Use workdir argument to change the process working directory.
+// Use env argument to provide additional environment variables in the form "key=value".
 func (tf *TF) Exec(workdir string, env []string, name string, args ...string) error {
 	cmd := exec.CommandContext(tf.Context(), name, args...) //nolint:gosec // yes, this runs a subprocess
 	cmd.Dir = workdir
@@ -13,8 +17,8 @@ func (tf *TF) Exec(workdir string, env []string, name string, args ...string) er
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, env...)
 
-	cmd.Stderr = tf.Writer()
-	cmd.Stdout = tf.Writer()
+	cmd.Stderr = tf.Output()
+	cmd.Stdout = tf.Output()
 
 	cmdStr := strings.Join(append([]string{name}, args...), " ")
 	tf.Logf("Exec: %s", cmdStr)
--- tf.go
+++ tf.go
@@ -7,6 +7,13 @@ import (
 	"runtime"
 )
 
+// TF is a type passed to Task's Command function to manage task state.
+//
+// A Task ends when its Command function returns or calls any of the methods
+// FailNow, Fatal, Fatalf, SkipNow, Skip, or Skipf.
+//
+// All methods must be called only from the goroutine running the
+// Command function.
 type TF struct {
 	ctx     context.Context
 	name    string
@@ -15,54 +22,76 @@ type TF struct {
 	skipped bool
 }
 
+// Context returns the taskflows' run context.
 func (tf *TF) Context() context.Context {
 	return tf.ctx
 }
 
+// Name returns the name of the running task.
 func (tf *TF) Name() string {
 	return tf.name
 }
 
-func (tf *TF) Writer() io.Writer {
+// Output returns the io.Output used to print output.
+func (tf *TF) Output() io.Writer {
 	return tf.writer
 }
 
+// Logf formats its arguments according to the format, analogous to Printf,
+// and prints the text to Output. A final newline is added.
+// The text will be printed only if the task fails or taskflow is run in Verbose mode.
 func (tf *TF) Logf(format string, args ...interface{}) {
 	fmt.Fprintf(tf.writer, format+"\n", args...)
 }
 
+// Errorf is equivalent to Logf followed by Fail.
 func (tf *TF) Errorf(format string, args ...interface{}) {
 	tf.Logf(format, args...)
 	tf.Fail()
 }
 
+// Failed reports whether the function has failed.
 func (tf *TF) Failed() bool {
 	return tf.failed
 }
 
+// Fail marks the function as having failed but continues execution.
 func (tf *TF) Fail() {
 	tf.failed = true
 }
 
+// Fatalf is equivalent to Logf followed by FailNow.
 func (tf *TF) Fatalf(format string, args ...interface{}) {
 	tf.Logf(format, args...)
 	tf.FailNow()
 }
 
+// FailNow marks the function as having failed
+// and stops its execution by calling runtime.Goexit
+// (which then runs all deferred calls in the current goroutine).
+// It finishes the whole taskflow.
 func (tf *TF) FailNow() {
 	tf.Fail()
 	runtime.Goexit()
 }
 
+// Skipped reports whether the task was skipped.
 func (tf *TF) Skipped() bool {
 	return tf.skipped
 }
 
+// Skipf is equivalent to Logf followed by SkipNow.
 func (tf *TF) Skipf(format string, args ...interface{}) {
 	tf.Logf(format, args...)
 	tf.SkipNow()
 }
 
+// SkipNow marks the task as having been skipped
+// and stops its execution by calling runtime.Goexit
+// (which then runs all deferred calls in the current goroutine).
+// If a test fails (see Error, Errorf, Fail) and is then skipped,
+// it is still considered to have failed.
+// Taskflow will continue at the next task.
 func (tf *TF) SkipNow() {
 	tf.skipped = true
 	runtime.Goexit()
