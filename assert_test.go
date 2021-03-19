489d4372465d431930bf174799eb717b3cba1c85
Christian Haas <christian.haas@sevensuns.at>
2021-03-19 17:04:48 +0100

    add equal assertion

--- assert_test.go
+++ assert_test.go
@@ -1,6 +1,7 @@
 package taskflow_test
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -16,5 +17,12 @@ func assertContains(t testing.TB, value string, expected string) {
 	if !strings.Contains(value, expected) {
 		t.Helper()
 		t.Errorf("Have [%s], want [%s]", value, expected)
+	}
+}
+
+func assertEqual(t testing.TB, value interface{}, expected interface{}, message string) {
+	if !reflect.DeepEqual(value, expected) {
+		t.Helper()
+		t.Errorf("%s\nHave [%v], want [%v]", message, value, expected)
 	}
 }
