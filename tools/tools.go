a01c2b23b8d2bf22e00271623e27b898ec1b5850
Robert Pająk <pellared@hotmail.com>
2022-10-12 02:13:37 +0200

    Refine build pipeline (#211)

--- tools/tools.go
+++ tools/tools.go
@@ -10,6 +10,4 @@ package tools
 import (
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "golang.org/x/tools/cmd/goimports"
-	_ "mvdan.cc/gofumpt"
 )
