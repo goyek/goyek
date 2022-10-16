9ac0163ca9b4f0a7f1b4007d57c1507b5a6d2519
Robert Pająk <pellared@hotmail.com>
2022-10-16 11:29:31 +0200

    Use flag and middleware

--- build/main.go
+++ build/main.go
@@ -2,20 +2,47 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/goyek/goyek/v2"
+	"github.com/goyek/goyek/v2/middleware"
 )
 
+// directories used in repository
 const (
-	rootDir  = "."
-	buildDir = "build"
-	toolsDir = "tools"
+	dirRoot  = "."
+	dirBuild = "build"
+	dirTools = "tools"
+)
+
+// flags
+var (
+	verbose = flag.Bool("v", false, "print all tasks and tests as they are run")
 )
 
 var flow = &goyek.Flow{}
 
 func main() {
 	flow.SetDefault(all)
-	flow.Main(os.Args[1:])
+
+	flag.CommandLine.SetOutput(os.Stdout)
+	usage := func() {
+		fmt.Println("Usage of build: [flags] [--] [tasks]")
+		flow.Print()
+		fmt.Println("Flags:")
+		flag.PrintDefaults()
+	}
+	flow.Usage = usage
+	flag.Usage = usage
+
+	flag.Parse()
+
+	flow.Use(middleware.Reporter)
+	if !*verbose {
+		flow.Use(middleware.SilentNonFailed)
+	}
+
+	flow.Main(flag.Args())
 }
