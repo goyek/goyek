9245e57da2ce6b403c290dfd3d4026c679fe6880
Robert Pająk <pellared@hotmail.com>
2022-10-25 23:00:41 +0200

    Refine build pipeline

--- build/golint.go
+++ build/golint.go
@@ -6,7 +6,7 @@ var golint = goyek.Define(goyek.Task{
 	Name:  "golint",
 	Usage: "golangci-lint run --fix",
 	Action: func(tf *goyek.TF) {
-		if ok := Exec(tf, dirBuild, "go install github.com/golangci/golangci-lint/cmd/golangci-lint"); !ok {
+		if !Exec(tf, dirBuild, "go install github.com/golangci/golangci-lint/cmd/golangci-lint") {
 			return
 		}
 		Exec(tf, dirRoot, "golangci-lint run --fix")
